eaf35d56f6aac8b3053af040ba24f1c1a92c1330
eaktassssss <eaktasgithub@gmail.com>
2020-10-23 00:40:22 +0300

    Add project files.

---
+++ RabbitMQ.Consumer/Program.cs
@@ -0,0 +1,32 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace RabbitMQ.Consumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqps://rqsnzfbj:5DYTnOwAMXKArn0bR0yAGxVpZm78mP2O@toad.rmq.cloudamqp.com/rqsnzfbj");
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare("FirstQueue", false, false, false, null);
+                    var consumer = new EventingBasicConsumer(channel);
+                    consumer.Received += Consumer_Received;
+                    channel.BasicConsume(queue: "FirstQueue", autoAck: true, consumer: consumer);
+                }
+            }
+            Console.ReadLine();
+        }
+        private static void Consumer_Received(object sender, BasicDeliverEventArgs e)
+        {
+            var message = Encoding.UTF8.GetString(e.Body.ToArray());
+            Console.WriteLine($"Mesaj Alındı:{message}");
+        }
+    }
+}
---
+++ RabbitMQ.Publisher/Program.cs
@@ -0,0 +1,27 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace RabbitMQ.Publisher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string message = "Test Mesaj";
+            var factory = new ConnectionFactory();
+            factory.HostName = "localhost";
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    var result = channel.QueueDeclare("FirstQueue", false, false, false, null);
+                    var body = Encoding.UTF8.GetBytes(message);
+                    channel.BasicPublish(exchange: "", routingKey: "FirstQueue", null, body: body);
+                }
+                Console.WriteLine("Mesajınız gönderildi!");
+            }
+            Console.ReadLine();
+        }
+    }
+}
